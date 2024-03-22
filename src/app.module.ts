b0bb7d8190e1b902ebd7e75678a26201fb8b72d2
Patrick <patrickrocha5@outlook.com>
2024-03-22 13:02:48 -0700

    Added Auth, Company, User Module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, CompanyModule],
 })
 export class AppModule {}
