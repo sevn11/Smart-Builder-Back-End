63d12703acb6a5cf6a66e6f09af3c5a7c5d07acc
Patrick <patrickrocha5@outlook.com>
2024-03-25 13:13:34 -0700

    Added /Me API.

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+    constructor(private userService: UserService) {
+
+    }
+
+    @Get('/me')
+    me() {
+        return this.userService.me();
+    }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class UserService {
+    constructor(private databaseService: DatabaseService) {
+
+    }
+
+    async me() {
+        try {
+            const user = await this.databaseService.user.findFirst();
+            delete user.hash;
+            return user;
+        } catch (error) {
+            throw new InternalServerErrorException()
+        }
+    }
+}
