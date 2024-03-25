27b792cd3215b7aea5760d0ce0e816c302546685
Patrick <patrickrocha5@outlook.com>
2024-03-25 08:43:58 -0700

    Added Database Module and Basic Structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CompanyModule],
+  imports: [AuthModule, UserModule, CompanyModule, DatabaseModule],
 })
 export class AppModule {}
