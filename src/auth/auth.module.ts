3feef7c66f7fa6a1ab3fa61aa6a8b7033cdca03e
Patrick Rocha <patrickrocha5@outlook.com>
2024-03-27 08:58:42 -0700

    Added JWT Token.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService, JwtService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '3h' },
+    }),
+  ],
+  providers: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule { }
