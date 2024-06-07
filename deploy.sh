7651e3066e06660bb98b24fe2f6cb1ccb03efc66
Mathew <itekk@users.noreply.github.com>
2024-06-08 00:45:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@
 cd /home/ubuntu/Smart-Builder-Back-End
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 git pull origin $branch
+
+cd /home/ubuntu/Smart-Builder-Back-End
 pm2 restart smart-builder-backend
