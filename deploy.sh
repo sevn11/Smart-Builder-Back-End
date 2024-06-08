9780e1030c94d3517a03ce3dac8d2cd3c427184b
Mathew <itekk@users.noreply.github.com>
2024-06-08 14:21:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,13 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 # Pull the latest changes from the repository
 git pull origin $branch
-
+#stop app
+pm2 stop smart-builder-backend
+pm2 delete smart-builder-backend
+pm2 save --force
 # build application
+pnpm install
 pnpm build
-# Restart the application using pm2
-pm2 restart smart-builder-backend
+# start the application using pm2
+pm2 start ecosystem.config.js 
+pm2 save
