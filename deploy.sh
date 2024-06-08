c33892fe9aeda2f1cd21693bf94e99155c465a6b
Mathew <itekk@users.noreply.github.com>
2024-06-08 14:48:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Source the user's profile to ensure environment variables are loaded
+source /home/ubuntu/.profile
+
+# Explicitly set the path to include the Node and pm2 binaries
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.17.0/bin
+
+# Navigate to the application directory
+cd /home/ubuntu/Smart-Builder-Back-End
+
+git clean -fd
+git stash
+
+# Get the current branch name
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# Pull the latest changes from the repository
 git pull origin $branch
-pm2 restart smart-builder-backend
+#stop app
+pm2 stop smart-builder-backend
+pm2 delete smart-builder-backend
+pm2 save --force
+# build application
+pnpm install
+pnpm build
+# start the application using pm2
+pm2 start ecosystem.config.js 
+pm2 save
