e0d0615cbf76402e32cdf5358ed12baafaf5b428
Mathew <itekk@users.noreply.github.com>
2024-06-08 12:01:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ source /home/ubuntu/.profile
 cd /home/ubuntu/Smart-Builder-Back-End
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 git pull origin $branch
- /home/ubuntu/.nvm/versions/node/v18.17.0/bin/pm2 ls
- /home/ubuntu/.nvm/versions/node/v18.17.0/bin/pm2 restart smart-builder-backend
+/home/ubuntu/.nvm/versions/node/v18.17.0/bin/pm2 ls
+/home/ubuntu/.nvm/versions/node/v18.17.0/bin/pm2 restart smart-builder-backend
