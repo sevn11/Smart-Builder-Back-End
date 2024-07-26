70c0443ffd0549b39ddbd1e3ed3b9395f5dc3e76
Mathew Varghese <github@itekk.xyz>
2024-07-26 23:15:25 +0530

    Made questionnair template name editable under settings

--- src/questionnaire-template/questionnaire-template.controller.ts
+++ src/questionnaire-template/questionnaire-template.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
+import { QuestionnaireTemplateService } from './questionnaire-template.service';
+import { GetUser } from 'src/core/decorators';
+import { User } from '@prisma/client';
+import { CreateUpdateQuestionnaireTemplateDTO } from './validators/create-edit-questionnaire-template';
+import { JwtGuard } from 'src/core/guards';
 
-@Controller('questionnaire-template')
-export class QuestionnaireTemplateController {}
+
+@UseGuards(JwtGuard)
+@Controller('companies/:companyId/questionnaire-template')
+export class QuestionnaireTemplateController {
+    constructor(private questionnaireTemplateService: QuestionnaireTemplateService) {
+
+    }
+
+    @Post()
+    createQuestionnaireTemplate(@GetUser() user: User, @Param('companyId', ParseIntPipe) companyId: number, @Body() body: CreateUpdateQuestionnaireTemplateDTO) {
+        return this.questionnaireTemplateService.createQuestionnaireTemplate(user, companyId, body);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get()
+    getQuestionnaireTemplateList(@GetUser() user: User, @Param('companyId', ParseIntPipe) companyId: number) {
+        return this.questionnaireTemplateService.getQuestionnaireTemplateList(user, companyId);
+    }
+
+    @Patch(':templateId')
+    @HttpCode(HttpStatus.OK)
+    updateQuestionnaireTemplate(
+        @GetUser() user: User,
+        @Param('companyId', ParseIntPipe) companyId: number,
+        @Param('templateId', ParseIntPipe) templateId: number,
+        @Body() body: CreateUpdateQuestionnaireTemplateDTO,
+    ) {
+        return this.questionnaireTemplateService.updateQuestionnaireTemplate(user, companyId, templateId, body);
+    }
+
+
+}
--- src/questionnaire-template/questionnaire-template.service.ts
+++ src/questionnaire-template/questionnaire-template.service.ts
@@ -1,4 +1,177 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+import { CreateUpdateQuestionnaireTemplateDTO } from './validators/create-edit-questionnaire-template';
+import { PrismaErrorCodes, ResponseMessages, UserTypes } from 'src/core/utils';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
-export class QuestionnaireTemplateService {}
+export class QuestionnaireTemplateService {
+
+    constructor(private databaseService: DatabaseService) {
+
+    }
+
+
+    async createQuestionnaireTemplate(user: User, companyId: number, body: CreateUpdateQuestionnaireTemplateDTO) {
+        try {
+            // Check if User is Admin of the Company.
+            if (user.userType == UserTypes.ADMIN || (user.userType == UserTypes.BUILDER && user.companyId === companyId)) {
+                let company = await this.databaseService.company.findUnique({
+                    where: {
+                        id: companyId,
+                        isDeleted: false
+                    }
+                });
+                if (!company) {
+                    throw new ForbiddenException("Action Not Allowed");
+                }
+
+
+                let template = await this.databaseService.questionnaireTemplate.create({
+                    data: {
+                        name: body.name,
+                        companyId: company.id,
+                    },
+                    omit: {
+                        isDeleted: true
+                    },
+
+                });
+                return { template }
+
+            } else {
+                throw new ForbiddenException("Action Not Allowed");
+            }
+        } catch (error) {
+            console.log(error);
+            // Database Exceptions
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code == PrismaErrorCodes.UNIQUE_CONSTRAINT_ERROR)
+                    throw new BadRequestException(ResponseMessages.UNIQUE_EMAIL_ERROR);
+                else {
+                    console.log(error.code);
+                }
+            } else if (error instanceof ForbiddenException) {
+                throw error;
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+
+    async getQuestionnaireTemplateList(user: User, companyId: number) {
+
+        try {
+            // Check if User is Admin of the Company.
+            if (user.userType == UserTypes.ADMIN || (user.userType == UserTypes.BUILDER && user.companyId === companyId)) {
+                let company = await this.databaseService.company.findUnique({
+                    where: {
+                        id: companyId,
+                        isDeleted: false
+                    }
+                });
+                if (!company) {
+                    throw new ForbiddenException("Action Not Allowed");
+                }
+
+                let templates = await this.databaseService.questionnaireTemplate.findMany({
+                    where: {
+                        companyId,
+                        isDeleted: false,
+                    },
+
+                    omit: {
+                        isDeleted: true
+                    },
+
+                });
+
+                return { templates }
+            } else {
+                throw new ForbiddenException("Action Not Allowed");
+            }
+        } catch (error) {
+            console.log(error);
+            // Database Exceptions
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code == PrismaErrorCodes.UNIQUE_CONSTRAINT_ERROR)
+                    throw new BadRequestException(ResponseMessages.UNIQUE_EMAIL_ERROR);
+                else {
+                    console.log(error.code);
+                }
+            } else if (error instanceof ForbiddenException) {
+                throw error;
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+
+    async updateQuestionnaireTemplate(user: User, companyId: number, templateId: number, body: CreateUpdateQuestionnaireTemplateDTO) {
+        try {
+            // Check if User is Admin of the Company.
+            if (user.userType == UserTypes.ADMIN || (user.userType == UserTypes.BUILDER && user.companyId === companyId)) {
+                let company = await this.databaseService.company.findUnique({
+                    where: {
+                        id: companyId,
+                        isDeleted: false
+                    }
+                });
+                if (!company) {
+                    throw new ForbiddenException("Action Not Allowed");
+                }
+                let template = await this.databaseService.questionnaireTemplate.findUniqueOrThrow({
+                    where: {
+                        id: templateId,
+                        companyId,
+                        isDeleted: false,
+                    },
+                    omit: {
+                        isDeleted: true
+                    },
+
+                });
+
+                template.name = body.name;
+
+
+
+                // updating the template
+                let updatedTemplate = await this.databaseService.questionnaireTemplate.update({
+                    where: {
+                        id: templateId,
+                        companyId,
+                        isDeleted: false
+                    },
+                    data: {
+                        ...template
+                    }
+                });
+
+                return { updatedTemplate }
+            } else {
+                throw new ForbiddenException("Action Not Allowed");
+            }
+        } catch (error) {
+            console.log(error);
+            // Database Exceptions
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code == PrismaErrorCodes.UNIQUE_CONSTRAINT_ERROR)
+                    throw new BadRequestException(ResponseMessages.UNIQUE_EMAIL_ERROR);
+                else {
+                    console.log(error.code);
+                }
+            } else if (error instanceof ForbiddenException) {
+                throw error;
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+
+
+}
