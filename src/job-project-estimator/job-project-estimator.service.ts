a47c7c24260a445243aed1f3d08281c42187ca73
Mathew Varghese <github@itekk.xyz>
2024-08-15 20:42:12 +0530

    Set default value for change order invoice id.

--- src/job-project-estimator/job-project-estimator.service.ts
+++ src/job-project-estimator/job-project-estimator.service.ts
@@ -393,15 +393,44 @@ export class JobProjectEstimatorService {
                         ...projectEstimatorData
                     }
                 });
-                // insert invoice id
-                await this.databaseService.jobProjectEstimator.update({
-                    where: {
-                      id: projectEstimator.id,
-                    },
-                    data: {
-                      invoiceId: projectEstimator.id + 1000,
-                    },
-                })
+                // insert invoice id for change orders
+                if(projectEstimator.item === 'Change Order') {
+                    // check for existing invoice ids
+                    let items = await this.databaseService.jobProjectEstimator.findMany({
+                        where: {
+                            jobProjectEstimatorHeaderId: accountingHeader.id,
+                            item: 'Change Order',
+                            id: { not: projectEstimator.id }
+                        },
+                        orderBy: {
+                            invoiceId: 'desc',
+                        },
+                    });
+
+                    if(items.length == 0) {
+                        await this.databaseService.jobProjectEstimator.update({
+                            where: {
+                                id: projectEstimator.id,
+                            },
+                            data: {
+                                invoiceId: 1100,
+                            },
+                        })
+                    }
+                    else {
+                        let highestInvoiceId = items[0].invoiceId;
+                        if(highestInvoiceId) {
+                            await this.databaseService.jobProjectEstimator.update({
+                                where: {
+                                    id: projectEstimator.id,
+                                },
+                                data: {
+                                    invoiceId: highestInvoiceId + 1,
+                                },
+                            });
+                        }
+                    }
+                }
                 
                 return { projectEstimator }
             } else {
