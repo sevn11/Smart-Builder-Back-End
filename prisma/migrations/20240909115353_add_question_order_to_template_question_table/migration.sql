b232b3cc4dfad8e4a5f2484ee2d93e47a86b500b
Mathew Varghese <github@itekk.xyz>
2024-09-10 08:40:04 -0400

    Fix migration

--- prisma/migrations/20240909115353_add_question_order_to_template_question_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `question_order` to the `template_question` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "template_question" ADD COLUMN "question_order" INTEGER DEFAULT 0;
---
+++ prisma/migrations/20240910122257_add_question_order_to_template_question_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "template_question" ADD COLUMN     "question_order" INTEGER NOT NULL DEFAULT 0;
