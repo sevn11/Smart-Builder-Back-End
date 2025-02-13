d8ed395ea52097e242c0244a149c8c4a1067408a
Mathew Varghese <github@itekk.xyz>
2025-02-13 16:58:35 +0530

    Project contractor details order change.

--- src/job-contractor/job-contractor.service.ts
+++ src/job-contractor/job-contractor.service.ts
@@ -281,11 +281,35 @@ export class JobContractorService {
                                 }
                             }),
                         ]);
-                        let formattedDetails = [
-                            ...clientCategoryDetails.map(clientCategory => ({
+                        // Sorting the data based on template type
+                        let linkToQuestionnaireItems = [];
+                        let linkToInitialSelectionItems = [];
+                        let linkToPaintSelectionItems = [];
+
+                        // Loop through each category to filter questions based on the priority
+                        clientCategoryDetails.forEach(category => {
+                            category.ClientTemplateQuestion.forEach(question => {
+                                if (question.linkToQuestionnaire) {
+                                    linkToQuestionnaireItems.push({ ...question, category });
+                                } else if (question.linkToInitalSelection) {
+                                    linkToInitialSelectionItems.push({ ...question, category });
+                                } else if (question.linkToPaintSelection) {
+                                    linkToPaintSelectionItems.push({ ...question, category });
+                                }
+                            });
+                        });
+
+                        const allItems = [
+                            ...linkToQuestionnaireItems,
+                            ...linkToInitialSelectionItems,
+                            ...linkToPaintSelectionItems
+                        ];
+                        let formattedDetails = clientCategoryDetails.map(clientCategory => {
+                            const categoryQuestions = allItems.filter(item => item.category.id === clientCategory.id);
+                            return {
                                 category: clientCategory.id,
                                 categoryName: clientCategory.name,
-                                questions: clientCategory.ClientTemplateQuestion.map(question => {
+                                questions: categoryQuestions.map(question => {
                                     let answer: any;
                                     if (question.questionType === "Allowance") {
                                         const answerText = question?.answer?.answerText ?? "0";
@@ -309,8 +333,8 @@ export class JobContractorService {
                                         answer
                                     };
                                 })
-                            }))
-                        ];
+                            }
+                        });
 
                         let htmlContent = await this.generateDetailsHtml(jobDetails, formattedDetails, contractor.phase);
                         // Generate pdf from HTML and add as attachment
