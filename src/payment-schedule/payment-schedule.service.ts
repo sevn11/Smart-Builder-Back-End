726f588caf219e81885dd552baf51d79bfea18fb
Mathew Varghese <github@itekk.xyz>
2025-07-29 05:01:15 -0400

    Fix draw request sorting wrt. date

--- src/payment-schedule/payment-schedule.service.ts
+++ src/payment-schedule/payment-schedule.service.ts
@@ -31,7 +31,7 @@ export class PaymentScheduleService {
                                 isDeleted: false
                             },
                             orderBy: {
-                                paymentDate: "asc"
+                                id: 'asc'
                             }
                         },
                     },
